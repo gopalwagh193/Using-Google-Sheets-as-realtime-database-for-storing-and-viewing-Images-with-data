24bd67ce0ac8755a655915c179e47fbbbc0a75ab
Gopal Wagh <54711990+gopalwagh193@users.noreply.github.com>
2020-07-15 01:33:33 -0700

    Create MainActivaty.java

---
+++ MainActivaty.java
@@ -0,0 +1,45 @@
+package com.example.gsheetsimage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button addUser,viewUser;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); addUser=(Button)findViewById(R.id.btn_add_user);
+        viewUser=(Button)findViewById(R.id.btn_list_user);
+
+        addUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(),AddUser.class);
+                startActivity(intent);
+
+            }
+        });
+
+        viewUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),UserList.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
+
+
+
+
+    }
+}
