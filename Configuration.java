a09d1eaf36181fd75fdd4286a2bda718193b8b24
Gopal Wagh <54711990+gopalwagh193@users.noreply.github.com>
2020-07-15 01:32:45 -0700

    Create Configuration.java

---
+++ Configuration.java
@@ -0,0 +1,18 @@
+package com.example.gsheetsimage;
+public class Configuration {
+
+    public static final String APP_SCRIPT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxdHhz9sA6DLSAP8V-pOpiMSM2ov8YTtJ__nencJxnEdsdjOdk/exec";
+    public static final String ADD_USER_URL = APP_SCRIPT_WEB_APP_URL;
+    public static final String LIST_USER_URL = APP_SCRIPT_WEB_APP_URL+"?action=readAll";
+
+    public static final String KEY_ID = "uId";
+    public static final String KEY_NAME = "uName";
+    public static final String KEY_IMAGE = "uImage";
+    public  static final String KEY_ACTION = "action";
+
+    public static final String KEY_USERS = "records";
+
+
+
+
+}
